2d7c819bbae63d5128c63a55917cb837136bd1d6
Evaldas88 <e.skackauskas@gmail.com>
2022-11-17 10:46:03 +0200

    first commit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,17 +3,15 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class Cors
 {
-     
-  
     public function handle($request, Closure $next)
-  {
-    return $next($request)
-    ->header('Access-Control-Allow-Origin', '*')
-    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type,X-Token-Auth, Authorization');
-  }
+    {
+
+          return $next($request)
+              ->header('Access-Control-Allow-Origin', '*')
+              ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+              ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type,X-Token-Auth, Authorization');
+    }
 }
